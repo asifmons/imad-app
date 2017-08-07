c0051a9235401079e265c087d0abcf995d2f5bcc
asifmons <30744224+asifmons@users.noreply.github.com>
2017-08-07 21:21:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,11 +2,11 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='new value';
 var img=document.getElementById('madi');
+var marginLeft=0;
 function moveRight(){
 marginLeft=marginLeft+10;
-    
+ img.style.marginLeft=marginleft+'px';   
 }
 img.onclick=function(){
-img.style.marginLeft='100px';
-var interval=setInterval(moveRight,100)
+var interval=setInterval(moveRight,100);
 };
