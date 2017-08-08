181f6bccf53be012e62baeb9d1c336a30d77637e
asifmons <30744224+asifmons@users.noreply.github.com>
2017-08-08 22:50:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,7 +15,7 @@ button.onclick=function(){
 };
 var nameInput=document.getElemenrById('name');
 var name=nameInput.value;
-var submit=document.getElemenrById('submit_btn');
+var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
     var names=['name1','name2',' name3','name4']
     var list ='';
