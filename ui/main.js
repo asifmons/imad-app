67383edbd9467a65b49ae29f3695391c5b149c1c
asifmons <30744224+asifmons@users.noreply.github.com>
2017-08-08 22:48:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,3 +13,16 @@ button.onclick=function(){
     request.open('GET','http://asifshaji777.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+var nameInput=document.getElemenrById('name');
+var name=nameInput.value;
+var submit=document.getElemenrById('submit_btn');
+submit.onclick=function(){
+    var names=['name1','name2',' name3','name4']
+    var list ='';
+    for(var i=0;i<names.length;i++){
+        list+='<li>'+names[i]+'</li>'
+    }
+    var ul=document.getElementById('namelist');
+    ul.innerHTML=list;
+    
+}
