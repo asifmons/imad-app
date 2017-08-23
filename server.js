c1c5e54ea28ac592b5a6a12e6356fa6ed8fe13a6
asifmons <30744224+asifmons@users.noreply.github.com>
2017-08-23 21:08:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,98 +3,28 @@ var morgan = require('morgan');
 var path = require('path');
 
 var app = express();
-app.use(morgan ('combined'));
-var articles={
- 'article-one':{
-title:'Article One | Asif',
-heading:'Article One',
-date:'Aug 6,2017',
-    content:` <p>
-                this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.
-            </p>
-            <p>
-                this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.
-            </p>
-            <p>
-                this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.
-            </p>`
-},
-'article-two':{
-    title:'Article Two | Asif',
-heading:'Article Two',
-date:'Aug 7,2017',
-    content:` <p>
-                this is the content for my second article.
-                </p>`},
-'article-three':{
-    title:'Article Three | Asif',
-heading:'Article Three',
-date:'Aug 8,2017',
-    content:` <p>
-                this is the content for my third article.
-                </p>`}
-};
-function createTemplate(data){
-    var title=data.title;
-    var date=data.date;
-    var heading=data.heading;
-    var content=data.content;
-var htmlTemplate=`<html>
-     <head>
-        <title>
-            ${title}
-        </title>
-        <meta name="viewport" content="width=device-width,initial scale=1"/>
-         <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-    <body>
-        <div class="container">
-        <div>
-            <a href="/">Home</a>
-        </div>
-        <hr/>
-        <h3>
-            ${heading} 
-        </h3>
-        <div>
-            ${date}
-        </div>
-        <div>
-            ${content}
-        </div>
-        </div>
-    </body>
-</html>`;
-return htmlTemplate;
-}
+app.use(morgan('combined'));
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-var counter=0;
-app.get('/counter', function (req, res) {
-  counter =counter+1;
-  res.send(counter.toString());
+app.get('/article-one',function(req,res){
+   res.send('Article one requested and will be served here'); 
 });
-app.get('/:articleName',function(req,res){
-    var articleName=req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+app.get('/article-two',function(req,res){
+   res.send('Article two requested and will be served here'); 
+});
+app.get('/article-three',function(req,res){
+   res.send('Article three requested and will be served here'); 
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-var names=[];
-app.get('/submit-name',function(req,res){
-    var name=req.params.name;
-    names.push(name);
-    res.send(JSON.stringify(names));
-})
+
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
